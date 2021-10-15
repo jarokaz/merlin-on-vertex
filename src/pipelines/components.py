c0a772b684048961dd2e5603fb9adb1142e4a535
Khalid Salama <khalid.m.salama@gmail.com>
2021-10-15 17:49:38 +0100

    Update components.py

--- src/pipelines/components.py
+++ src/pipelines/components.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Preprocessing components"""
+"""KFP components."""
 
 from kfp.v2 import dsl
 from kfp.v2.dsl import (
