7f7df29a85922a52f346f83eaad979a816bf9ee6
khalid Salama <khalid.m.salama@gmail.com>
2021-10-24 10:22:19 +0000

    Update config

--- src/pipelines/config.py
+++ src/pipelines/config.py
@@ -19,7 +19,11 @@ import os
 PROJECT_ID = os.getenv("PROJECT_ID", "")
 REGION = os.getenv("REGION", "us-central1")
 BUCKET = os.getenv("BUCKET", "")
-WORKSPACE = os.getenv("NVT_IMAGE_URI", f"gs://{BUCKET}")
+
+VERSION = "v01"
+MODEL_DISPLAY_NAME = os.getenv("MODEL_DISPLAY_NAME", f"criteo-merlin-recommender-{VERSION}")
+WORKSPACE = os.getenv("WORKSPACE", f'gs://{BUCKET}/{MODEL_DISPLAY_NAME}')
+EXPORTED_MODEL_DIR = os.getenv("EXPORTED_MODEL_DIR", os.path.join(WORKSPACE, 'EXPORTED_MODEL_DIR')
 
 WORKFLOW_PATH = os.getenv(
     "WORKFLOW_PATH", 
@@ -34,6 +38,7 @@ TRANSFORMED_OUTPUT_DIR = os.getenv(
     os.path.join(WORKSPACE, 'transformed_data')
 ) # Location of the transformed data files.
 
+
 BQ_DATASET_NAME = os.getenv("BQ_DATASET_NAME", "criteo")
 BQ_LOCATION = os.getenv("BQ_LOCATION", "US")
 BQ_TRAIN_TABLE_NAME = os.getenv("BQ_TRAIN_TABLE_NAME", "train")
@@ -43,10 +48,9 @@ PREPROCESS_CSV_PIPELINE_NAME = os.getenv("PREPROCESS_CSV_PIPELINE_NAME", "nvt-cs
 PREPROCESS_BQ_PIPELINE_NAME = os.getenv("PREPROCESS_BQ_PIPELINE_NAME", "nvt-bq-pipeline")
 TRAINING_PIPELINE_NAME = os.getenv("TRAINING_PIPELINE_NAME", "merlin-e2e-pipeline")
 
-MODEL_DISPLAY_NAME = os.getenv("MODEL_DISPLAY_NAME", "criteo-merlin-recommender")
-
 NVT_IMAGE_URI = os.getenv("NVT_IMAGE_URI",  f"gcr.io/{PROJECT_ID}/nvt_preprocessing")
 HUGECTR_IMAGE_URI = os.getenv("HUGECTR_IMAGE_URI",  f"gcr.io/{PROJECT_ID}/hugectr_training")
+TRITON_IMAGE_URI = os.getenv("HUGECTR_IMAGE_URI",  f"gcr.io/{PROJECT_ID}/triton_serving")
 
 MEMORY_LIMIT = os.getenv("MEMORY_LIMIT", "120G")
 CPU_LIMIT = os.getenv("CPU_LIMIT", "32")
@@ -54,6 +58,7 @@ GPU_LIMIT = os.getenv("GPU_LIMIT", "4")
 GPU_TYPE = os.getenv("GPU_TYPE", "nvidia-tesla-t4")
 
 MACHINE_TYPE = os.getenv("MACHINE_TYPE", "a2-highgpu-4g")
+REPLICA_COUNT = os.getenv("REPLICA_COUNT", "1")
 ACCELERATOR_TYPE = os.getenv("ACCELERATOR_TYPE", "NVIDIA_TESLA_A100")
 ACCELERATOR_NUM = os.getenv("ACCELERATOR_NUM", "4")
 NUM_WORKERS = os.getenv("NUM_WORKERS", "4")
