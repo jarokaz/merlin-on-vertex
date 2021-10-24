10032b6c1ab72a97980e6699f0680eea8e486699
khalid Salama <khalid.m.salama@gmail.com>
2021-10-24 08:57:16 +0000

    Update comments

--- src/pipelines/preprocessing_pipelines.py
+++ src/pipelines/preprocessing_pipelines.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Preprocessing pipeline"""
+"""Preprocessing pipelines."""
 
 from . import components
 from kfp.v2 import dsl
