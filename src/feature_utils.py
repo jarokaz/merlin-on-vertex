a844d89bcf7c267ba0831a9d613f65ee6a3b1dec
ksalama <khalid.m.salama@gmail.com>
2021-10-26 10:18:11 +0000

    Add columns info in a separate module

---
+++ src/feature_utils.py
@@ -0,0 +1,49 @@
+# Copyright 2021 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#            http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+"""Dataset features."""
+
+import numpy as np
+
+def get_criteo_col_dtypes() -> Dict[str,Union[str, np.int32]]:
+    '''Returns a dict mapping column names to numpy dtype.
+    This function is specific to Criteo Dataset'''
+    # Specify column dtypes. Note that "hex" means that
+    # the values will be hexadecimal strings that should
+    # be converted to int32
+    col_dtypes = {}
+
+    col_dtypes["label"] = np.int32
+    for x in ["I" + str(i) for i in range(1, 14)]:
+        col_dtypes[x] = np.int32
+    for x in ["C" + str(i) for i in range(1, 27)]:
+        col_dtypes[x] = 'hex'
+
+    return col_dtypes
+
+
+def categorical_columns():
+    columns = []
+    for col_name, col_type in get_criteo_col_dtypes().items:
+        if col_type == np.int32:
+            columns.append(col_name)
+    return columns
+
+def numerical_columns():
+    columns = []
+    for col_name, col_type in get_criteo_col_dtypes().items:
+        if col_type == 'hex':
+            columns.append(col_name)
+    return columns
+
+def label_columns():
+    return ['label']
--- src/preprocessing/etl.py
+++ src/preprocessing/etl.py
@@ -1,43 +1,38 @@
+# Copyright 2021 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#            http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+"""Data preprocessing."""
+
+
 from typing import Dict, Union
 
 import numpy as np
 import fsspec
 import os
 
-try:
-    import nvtabular as nvt
-    from nvtabular.utils import device_mem_size
-    from nvtabular.io.shuffle import Shuffle
-    from nvtabular.ops import (
-        Categorify,
-        Clip,
-        FillMissing,
-        Normalize,
-    )
+import nvtabular as nvt
+from nvtabular.utils import device_mem_size
+from nvtabular.io.shuffle import Shuffle
+from nvtabular.ops import (
+    Categorify,
+    Clip,
+    FillMissing,
+    Normalize,
+)
 
-    from dask_cuda import LocalCUDACluster
-    from dask.distributed import Client
-except:
-    pass
+from dask_cuda import LocalCUDACluster
+from dask.distributed import Client
 
 from google.cloud import bigquery
-
-
-def get_criteo_col_dtypes() -> Dict[str,Union[str, np.int32]]:
-    '''Returns a dict mapping column names to numpy dtype.
-    This function is specific to Criteo Dataset'''
-    # Specify column dtypes. Note that "hex" means that
-    # the values will be hexadecimal strings that should
-    # be converted to int32
-    col_dtypes = {}
-
-    col_dtypes["label"] = np.int32
-    for x in ["I" + str(i) for i in range(1, 14)]:
-        col_dtypes[x] = np.int32
-    for x in ["C" + str(i) for i in range(1, 27)]:
-        col_dtypes[x] = 'hex'
-
-    return col_dtypes
 
 
 def create_csv_dataset(
