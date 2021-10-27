fcd930ef9455f599398d2c98be24d1e74232fbbf
Diman Ghazi <dimanghazi@google.com>
2021-10-27 06:21:34 +0000

    incorporating container changes

--- src/serving/inference.py
+++ src/serving/inference.py
@@ -18,6 +18,42 @@ from tritonclient.utils import np_to_triton_dtype
 import numpy as np
 import json
 
+
+def get_inference_input(data, binary_data):
+    """An object of InferInput class is used to describe
+    input tensor for an inference request.
+    Parameters
+    ----------
+    "name" : str
+        The name of input whose data will be described by this object
+    "shape" : list
+        The shape of the associated input.
+    "datatype" : str
+        The datatype of the associated input.
+    ----------
+    Based on the data format:
+    Json:
+        "data" : array
+            The data vector
+    
+    Binary:
+        "parameters": {
+            "binary_data_size": int
+         }
+    """
+    inputs = []
+
+    col_names = list(data.columns)
+    col_dtypes = [np.int32] * len(col_names)
+
+    for i, col in enumerate(data.columns):
+        d = data[col].astype(col_dtypes[i])
+        d = d.values.reshape(len(d), 1)
+        inputs.append(httpclient.InferInput(col_names[i], d.shape, np_to_triton_dtype(col_dtypes[i])))
+        inputs[i].set_data_from_numpy(d, binary_data)
+        
+    return inputs
+
 def get_inference_request(inputs, request_id):
     infer_request = {}
     if request_id != "":
@@ -43,16 +79,3 @@ def get_inference_request(inputs, request_id):
         return request_body, json_size
 
     return infer_request, request_body, None
-
-def get_inference_input(data, binary_data):
-    inputs = []
-
-    col_names = list(data.columns)
-    col_dtypes = [np.int32] * len(col_names)
-
-    for i, col in enumerate(data.columns):
-        d = data[col].astype(col_dtypes[i])
-        d = d.values.reshape(len(d), 1)
-        inputs.append(httpclient.InferInput(col_names[i], d.shape, np_to_triton_dtype(col_dtypes[i])))
-        inputs[i].set_data_from_numpy(d, binary_data)
-    return inputs
